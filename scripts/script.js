b06e8eded9175a48bd9659b6b0849072af8f901d
ahaddoz <45543027+ahaddoz@users.noreply.github.com>
2025-11-16 16:46:17 +0100

    regel 1177 error

--- scripts/script.js
+++ scripts/script.js
@@ -57,18 +57,6 @@ function sluitMenu() {
   }
 }
 
-/**********************************/
-/* bonus: menu sluiten met escape */
-/**********************************/
-// window.onkeydown = handleKeydown;
-
-// function handleKeydown(event) {
-//   if (event.key == "Escape") {
-//     var deNav = document.querySelector("nav");
-//     deNav.classList.remove("toonMenu");
-//   }
-// }
-
 /* MELDINGSBALK */
 
 var messages = [
